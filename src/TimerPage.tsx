d3f6341a173f473a1901ad39c363725f1c82b513
yongjoon <yongjoon@bearrobotics.ai>
2021-02-21 22:57:30 +0900

    Remove unused lines and files

--- src/TimerPage.tsx
+++ src/TimerPage.tsx
@@ -6,8 +6,6 @@ import Timer, {
   TimerStopButton,
 } from './Timer';
 import Todo, { TodoItem } from './Todo';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import TodoFormProvider, {
   DecreaseSprintButton,
   EstimationSprintInput,
@@ -21,24 +19,11 @@ const SHORT_BREAK = 300;
 const LONG_BREAK = 900;
 const WORK_TIME = 1500;
 function TimerPage() {
-  const [sprintSelection, setSprintSelection] = useState<number>(0);
   const [todos, setTodos] = useState<Array<TodoItem>>([]);
   const [timer, setTimer] = useState<number>(WORK_TIME);
   const [breaking, setBreaking] = useState<boolean>(false);
   const [round, setRound] = useState<number>(1);
 
-  const handleSprintUp = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    setSprintSelection((s) => s + 1);
-  };
-  const handleSprintDown = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    setSprintSelection((s) => s - 1);
-  };
-  const handleSprintChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setSprintSelection(Number(e.target.value));
-  };
   const handleTimerEnd = () => {
     if (breaking) {
       setTimer(1500);
@@ -53,17 +38,7 @@ function TimerPage() {
       setRound((r) => r + 1);
     }
   };
-  const createNewTodo = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    const { title, sprint } = e.target as typeof e.target & {
-      title: { value: string };
-      sprint: { value: number };
-    };
-    setTodos([
-      ...todos,
-      { id: todos.length, title: title.value, sprint: sprint.value },
-    ]);
-  };
+
   const submitNewTodo = (form: TodoFormState) => {
     if (form.title === '' || form.sprint <= 0) return false;
     setTodos([
